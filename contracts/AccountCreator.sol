e33b244c6534a3d5e165615eb6b3e80e7c03fd77
chesterchen2010 <chesterchen2010@hotmail.com>
2020-05-28 11:16:23 +0800

    clean

--- contracts/AccountCreator.sol
+++ contracts/AccountCreator.sol
@@ -9,20 +9,11 @@ contract AccountCreator is MultiOwned {
     address public logicManager;
     address public accountStorage;
     address public accountImpl;
-    // address[] public logics;
 
     // *************** Events *************************** //
     event AccountCreated(address indexed wallet, address[] keys, address[] backups);
     event AddressesSet(address mgr, address strg, address impl);
     event Closed(address indexed sender);
-
-    // *************** Constructor ********************** //
-    // constructor(address _mgr, address _storage, address _accountImpl) public {
-    //     logicManager = _mgr;
-    //     accountStorage = _storage;
-    //     accountImpl = _accountImpl;
-    //     // logics = _logics;
-    // }
 
     // *************** Internal Functions ********************* //
 
@@ -36,8 +27,6 @@ contract AccountCreator is MultiOwned {
     function createAccount(address[] calldata _keys, address[] calldata _backups) external onlyMultiOwners {
         AccountProxy accountProxy = new AccountProxy(accountImpl);
         initializeAccount(address(accountProxy), _keys, _backups);
-        // Account(address(accountProxy)).init(logicManager, accountStorage, LogicManager(logicManager).getAuthorizedLogics(), _keys, _backups);
-        // emit AccountCreated(address(accountProxy), _keys, _backups);
     }
 
     /**
