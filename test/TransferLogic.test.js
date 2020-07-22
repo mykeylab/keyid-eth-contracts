378b7d973bae49f575b16fad409ef41d87c7a3fe
clar <baobao04@gmail.com>
2020-07-22 17:03:52 +0800

    add transfer test

--- test/TransferLogic.test.js
+++ test/TransferLogic.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-
+const truffleAssert = require('truffle-assertions');
 const AccountStorage = artifacts.require("AccountStorage");
 const LogicManager = artifacts.require("LogicManager");
 const TransferLogic = artifacts.require("TransferLogic");
@@ -39,19 +39,6 @@ contract("TransferLogic", accounts => {
 		myNft = await MyNft.deployed();
 		baseAccountImp = await BaseAccount.deployed();
 
-		baseAccountProxy = await BaseAccountProxy.new(baseAccountImp.address); 
-		baseAccount = await BaseAccount.at(baseAccountProxy.address)
-
-		await baseAccount.init(logicManager.address, accountStorage.address, [transferLogic.address, commonStaticLogic.address], [account1, account2], [])
-
-	    await myToken.mint(baseAccount.address, 10000);
-		await myToken.mint(account3, 10000);
-		await myToken.approve(baseAccount.address, 500, {from:account3});
-
-	    await myNft.mint(baseAccount.address, 123);
-		await myNft.mint(account3, 456);
-		await myNft.approve(baseAccount.address, 456, {from:account3});
-
 		mgrOwner = await logicManager.owner();
 		
 		// console.log("accountstorage:", accountStorage.address);
@@ -61,31 +48,20 @@ contract("TransferLogic", accounts => {
 		// console.log("baseAccount:", baseAccount.address);
         // console.log("mytoken:", myToken.address);
         // console.log("mynft:", myNft.address);
-	});
-
-	it('check current logics of logicManager', async () => {
-		const a = await logicManager.authorized(transferLogic.address);
-		assert.equal(a, true, "Current logic not correct.");
-	});
-	
-	it('check manager of BaseAccount', async () => {
-		const mgr = await baseAccount.manager();
-		assert.equal(mgr, logicManager.address, "The mgr of baseAccount is not correct.");
-	});
-	
-	it('create account', async () => {
-		var keys = [account2,account3];
-
+    });
+    
+    beforeEach(async () => {
+        var keys1 = [account1,account2];
+        baseAccountImp = await BaseAccount.deployed();
+		baseAccountProxy = await BaseAccountProxy.new(baseAccountImp.address); 
+		baseAccount = await BaseAccount.at(baseAccountProxy.address)
+        await baseAccount.init(logicManager.address, accountStorage.address, [transferLogic.address, commonStaticLogic.address], keys1, [])
+        
+        var keys2 = [account2,account3];
 		baseAccountProxy2 = await BaseAccountProxy.new(baseAccountImp.address); 
 		baseAccount2 = await BaseAccount.at(baseAccountProxy2.address)
-		await baseAccount2.init(logicManager.address, accountStorage.address, [transferLogic.address, commonStaticLogic.address], keys, [])
-
-		var pk0 = await accountStorage.getKeyData(baseAccount2.address, 0);
-		var pk1 = await accountStorage.getKeyData(baseAccount2.address, 1);
-
-		assert.equal(pk0, keys[0], "account created with incorrect pubkey address 0.");
-		assert.equal(pk1, keys[1], "account created with incorrect pubkey address 1.");
-	});
+		await baseAccount2.init(logicManager.address, accountStorage.address, [transferLogic.address, commonStaticLogic.address], keys2, [])
+    }); 
 
 	it('transfer eth', async () => {
 		var pk1 = await accountStorage.getKeyData(baseAccount.address, 1);
@@ -131,6 +107,8 @@ contract("TransferLogic", accounts => {
 
   it('check erc20 token direct transfer', async () => {
 
+    await myToken.mint(baseAccount.address, 10000);
+
     var pk1 = await accountStorage.getKeyData(baseAccount.address, 1);
     let nonce = await getNonce();
     let signingKey = pk1;
@@ -169,6 +147,10 @@ contract("TransferLogic", accounts => {
   });
 
   it('check erc20 token approved transfer', async () => {
+
+    await myToken.mint(account3, 10000);
+    await myToken.approve(baseAccount.address, 500, {from:account3});
+
     var pk1 = await accountStorage.getKeyData(baseAccount.address, 1);
     let nonce = await getNonce();
     let signingKey = pk1;
@@ -210,13 +192,62 @@ contract("TransferLogic", accounts => {
     assert.equal(balanceBefore-balanceAfter, amount, "erc20 token approved transfer failed.");
   });
 
-  it('check erc721 token transfer', async () => {
+  it('check erc721 token transfer - transferFrom', async () => {
+    let tokenId = 100
+    await myNft.mint(baseAccount.address, tokenId);
 
     var pk1 = await accountStorage.getKeyData(baseAccount.address, 1);
     let nonce = await getNonce();
     let signingKey = pk1;
     let toAddr =   baseAccount2.address;
-    let tokenId = '123';
+    let emptyBytes = [];
+    let safe = false;
+
+    let sendData = web3.eth.abi.encodeFunctionCall({
+                    name: 'transferNft',
+                    type: 'function',
+                    inputs: [{
+                        type: 'address',
+                        name: 'from'
+                    },{
+                        type: 'address',
+                        name: 'to'
+                    },{
+                        type: 'address',
+                        name: 'nftContract'
+                    },{
+                        type: 'uint256',
+                        name: 'tokenId'
+                    },{
+                        type: 'bytes',
+                        name: 'data'
+                    },{
+                        type: 'bool',
+                        name: 'safe'
+                    }]
+                }, [baseAccount.address, toAddr, myNft.address, tokenId, emptyBytes, safe]);
+    
+    let msg = '0x1900' + transferLogic.address.slice(2) + sendData.slice(2) + nonce.toString('16').slice(2);
+    let hash = web3.utils.soliditySha3(msg);
+    let sig = await web3.eth.sign(hash,signingKey);
+    sig = fixSignature(sig);
+
+    await transferLogic.enter(sendData, sig, nonce);
+
+    var ownerAfter = await myNft.ownerOf(tokenId);
+    assert.equal(ownerAfter, toAddr, "erc721 token transfer failed.");
+  });
+
+
+  it('check erc721 token transfer - safeTransferFrom', async () => {
+
+    let tokenId = 101
+    await myNft.mint(baseAccount.address, tokenId);
+
+    var pk1 = await accountStorage.getKeyData(baseAccount.address, 1);
+    let nonce = await getNonce();
+    let signingKey = pk1;
+    let toAddr =   baseAccount2.address;
     let emptyBytes = [];
     let safe = true;
 
@@ -255,13 +286,17 @@ contract("TransferLogic", accounts => {
     assert.equal(ownerAfter, toAddr, "erc721 token transfer failed.");
   });
 
-  it('check erc721 token approved transfer', async () => {
 
+  it('check erc721 token approved transfer - transferFrom', async () => {
+
+    let tokenId = 400;
+    await myNft.mint(account3, tokenId);
+    await myNft.approve(baseAccount.address, tokenId, {from:account3});
+        
     var pk1 = await accountStorage.getKeyData(baseAccount.address, 1);
     let nonce = await getNonce();
     let signingKey = pk1;
     let toAddr =   baseAccount2.address;
-    let tokenId = '456';
     let emptyBytes = [];
     let safe = false;
 
@@ -303,6 +338,235 @@ contract("TransferLogic", accounts => {
     assert.equal(ownerAfter, toAddr, "erc721 token approved transfer failed.");
   });
 
+  it('check erc721 token approved transfer - safeTransferFrom', async () => {
+
+    let tokenId = 401;
+    await myNft.mint(account3, tokenId);
+    await myNft.approve(baseAccount.address, tokenId, {from:account3});
+        
+    var pk1 = await accountStorage.getKeyData(baseAccount.address, 1);
+    let nonce = await getNonce();
+    let signingKey = pk1;
+    let toAddr =   baseAccount2.address;
+    let emptyBytes = [];
+    let safe = true;
+
+    let sendData = web3.eth.abi.encodeFunctionCall({
+                    name: 'transferApprovedNft',
+                    type: 'function',
+                    inputs: [{
+                        type: 'address',
+                        name: 'approvedSpender'
+                    },{
+                        type: 'address',
+                        name: 'from'
+                    },{
+                        type: 'address',
+                        name: 'to'
+                    },{
+                        type: 'address',
+                        name: 'nftContract'
+                    },{
+                        type: 'uint256',
+                        name: 'tokenId'
+                    },{
+                        type: 'bytes',
+                        name: 'data'
+                    },{
+                        type: 'bool',
+                        name: 'safe'
+                    }]
+                }, [baseAccount.address, account3, toAddr, myNft.address, tokenId, emptyBytes, safe]);
+    
+    let msg = '0x1900' + transferLogic.address.slice(2) + sendData.slice(2) + nonce.toString('16').slice(2);
+    let hash = web3.utils.soliditySha3(msg);
+    let sig = await web3.eth.sign(hash,signingKey);
+    sig = fixSignature(sig);
+
+    await transferLogic.enter(sendData, sig, nonce);
+
+    var ownerAfter = await myNft.ownerOf(tokenId);
+    assert.equal(ownerAfter, toAddr, "erc721 token approved transfer failed.");
+  });
+
+
+  it('should only let asset key to transfer token', async () => {
+    var pk = await accountStorage.getKeyData(baseAccount.address, 0); // admin key
+
+    let toAddr = account2;
+    let signingKey = pk;
+    let nonce = await getNonce();
+    let amount = 1000  
+
+    await web3.eth.sendTransaction({from:account0, to:baseAccount.address, value:2000});
+
+    let data = web3.eth.abi.encodeFunctionCall({
+                    name: 'transferEth',
+                    type: 'function',
+                    inputs: [{
+                        type: 'address',
+                        name: 'from'
+                    },{
+                        type: 'address',
+                        name: 'to'
+                    },{
+                        type: 'uint256',
+                        name: 'amount'
+                    }]
+                }, [baseAccount.address, toAddr, amount]);
+
+    let msg = '0x1900' + transferLogic.address.slice(2) + data.slice(2) + (nonce.toString('16')).slice(2);
+    let hash = web3.utils.soliditySha3(msg);
+
+    let sig = await web3.eth.sign(hash, signingKey);
+    sig = fixSignature(sig);
+
+
+    await truffleAssert.reverts(
+        transferLogic.enter(data, sig, nonce)
+    );
+    
+});
+
+
+it('should fail to transfer eth over balance', async () => {
+    var pk = await accountStorage.getKeyData(baseAccount.address, 1); // asset key
+
+    let toAddr = account2;
+    let signingKey = pk;
+    let nonce = await getNonce();//32;
+    let amount = 1000  // transfer 1000 wei
+
+    // await web3.eth.sendTransaction({from:account0, to:baseAccount.address, value:2000});
+
+    let data = web3.eth.abi.encodeFunctionCall({
+                    name: 'transferEth',
+                    type: 'function',
+                    inputs: [{
+                        type: 'address',
+                        name: 'from'
+                    },{
+                        type: 'address',
+                        name: 'to'
+                    },{
+                        type: 'uint256',
+                        name: 'amount'
+                    }]
+                }, [baseAccount.address, toAddr, amount]);
+
+    let msg = '0x1900' + transferLogic.address.slice(2) + data.slice(2) + nonce.toString('16').slice(2);
+    let hash = web3.utils.soliditySha3(msg);
+
+    let sig = await web3.eth.sign(hash, signingKey);
+    sig = fixSignature(sig);
+
+
+    await truffleAssert.reverts(
+        transferLogic.enter(data, sig, nonce)
+    );
+    
+    
+});
+    
+
+
+it('should fail to transfer erc20 token over balance', async () => {
+
+    let mintAmount = 100
+    await myToken.mint(baseAccount.address, mintAmount);
+
+    var pk1 = await accountStorage.getKeyData(baseAccount.address, 1); 
+    let nonce = await getNonce();//64;
+    let signingKey = pk1;
+    let toAddr =   account2;
+    let amount = mintAmount + 1; 
+
+    let sendData = web3.eth.abi.encodeFunctionCall({
+                    name: 'transferErc20',
+                    type: 'function',
+                    inputs: [{
+                        type: 'address',
+                        name: 'from'
+                    },{
+                        type: 'address',
+                        name: 'to'
+                    },{
+                        type: 'address',
+                        name: 'token'
+                    },{
+                        type: 'uint256',
+                        name: 'amount'
+                    }]
+                }, [baseAccount.address, toAddr, myToken.address, amount]);
+    
+    let msg = '0x1900' + transferLogic.address.slice(2) + sendData.slice(2) + nonce.toString('16').slice(2);
+    let hash = web3.utils.soliditySha3(msg);
+    // console.log('hash:', hash);
+    // console.log('address1:', account1);
+    // console.log('address2:', account2);
+    let sig = await web3.eth.sign(hash, signingKey);
+    sig = fixSignature(sig);
+    // console.log(balanceBefore + " MTK");
+
+    await truffleAssert.reverts(
+        transferLogic.enter(sendData, sig, nonce)
+    );
+
+
+  });
+
+
+
+it('should fail to do approved transfer when amount is above allowance', async () => {
+
+    let allowance = 500
+    await myToken.mint(account3, 10000);
+    await myToken.approve(baseAccount.address, allowance, {from:account3});
+    
+    var pk1 = await accountStorage.getKeyData(baseAccount.address, 1);
+    let nonce = await getNonce();
+    let signingKey = pk1;
+    let toAddr =   account2;
+    let amount = allowance + 1; 
+
+    let sendData = web3.eth.abi.encodeFunctionCall({
+                    name: 'transferApprovedErc20',
+                    type: 'function',
+                    inputs: [{
+                        type: 'address',
+                        name: 'approvedSpender'
+                    },{
+                        type: 'address',
+                        name: 'from'
+                    },{
+                        type: 'address',
+                        name: 'to'
+                    },{
+                        type: 'address',
+                        name: 'token'
+                    },{
+                        type: 'uint256',
+                        name: 'amount'
+                    }]
+                }, [baseAccount.address, account3, toAddr, myToken.address, amount]);
+    
+    let msg = '0x1900' + transferLogic.address.slice(2) + sendData.slice(2) + (nonce.toString('16')).slice(2);
+    let hash = web3.utils.soliditySha3(msg);
+    // console.log('hash:', hash);
+    // console.log('address1:', account1);
+    // console.log('address2:', account2);
+    let sig = await web3.eth.sign(hash,signingKey);
+    sig = fixSignature(sig);
+    const balanceBefore = await myToken.balanceOf(account3);
+    // console.log(balanceBefore + " MTK");
+    
+    await truffleAssert.reverts(
+        transferLogic.enter(sendData, sig, nonce)
+    );
+
+
+});
+
 })
 
 async function getNonce() {
@@ -320,6 +584,8 @@ function fixSignature (signature) {
   const vHex = v.toString(16);
   return signature.slice(0, 130) + vHex;
 }
+
+
 
 function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
